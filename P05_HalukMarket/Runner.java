916f9a4a530646ac541786b2052b2a96520b877d
muhammedYusufAygun <ysfayg61@gmail.com>
2022-12-29 18:51:11 +0300

    yusuf

---
+++ P05_HalukMarket/Runner.java
@@ -0,0 +1,11 @@
+package JavaProjects.P05_HalukMarket;
+
+public class Runner {
+    public static void main(String[] args) {
+        System.out.println("Satici ekrani icin Kullanici Adi = muhammed Sifre = 6161");
+        System.out.println("Müsteri ekrani icin Kullanici Adi = yusuf Sifre = 5858");
+        KullaniciGiris.Kullanici();
+    }
+}
+
+
